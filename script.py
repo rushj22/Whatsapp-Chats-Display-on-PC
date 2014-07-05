f8ffbec1c18663ca36eef52a571794c094aada9b
Arushi Jain <arushi22@gmail.com>
2014-07-06 01:00:17 +0530

    Comments inserted. Image border.

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
-import sys,webbrowser,os
+import webbrowser,os,sys
 
-d=sys.argv[1]
+d = sys.argv[1]
 
 #storing chat file location in a variable
 for file in os.listdir(sys.argv[1]):
@@ -8,151 +8,160 @@ for file in os.listdir(sys.argv[1]):
 		d=os.path.join(d,file)
 
 #file reading
-fp=open(d,"r+")
+fp=open(d,"r")
 a=fp.read()
 
-template='''<html>
+name1=""
+template = """
+<html>
 <head>
-<title>Whatsapp Chats Display on PC</title>
-</head>
+<title>Your whatsapp chat</title>
 <style>
-li.a	{
-		list-style-type:none;
-		text-align:right;
-		margin-bottom:15px;
-	}
-li 		{marhin-bottom:15px;}
-
-li.b 	{
-		list-style-type:none;
-		text-align:left;
-		margin-bottom:15px;
-		
-		
-	}
-span.a	{
-		box-shadow: 0px 0px 2px black;
-    	border-radius: 5px;
-		background-color:rgb(214,248,183);
-		padding:6px;
-	}
-span.b  {
-		padding-left:10px;
-		box-shadow: 0px 0px 2px;
-    	border-radius: 5px;		
-		padding:6px;	
+li.b {
+	text-align:right;
 }
 
-li.date	{
-		list-style-type:none;
-    	border:1px solid; 
-		text-align:center;
-		padding:2px;
-		margin-bottom:15px;
-		
-
+.date {
+	border: 1px solid black;
+	padding: 3px;
+	text-align: center;
 }
 
-span.time	{
-		font-size:10;
+li.c {
+	text-align:left;
+	
+}
+
+li {
+	margin-bottom: 14px;
+}
+
+span.c {
+	box-shadow: 0 0 2px;
+	border-width: thin;
+	border-radius:6px;
+	padding:5px;
+
+	border-color:white;
+	
+
+	
+}
+
+span.time{
+		font-size:10px;
 		margin-left:6px;
 }
+span.b{
 
+	border:1px solid;
+	
+	border-radius:6px;
+	padding:5px;
+	border-color:rgb(214,248,183);
+	box-shadow: 0 0 2px;
+	background-color: rgb(214,248,183);
+}
 </style>
+</head>
 <body>
-<ul>
+<ul style="list-style-type:none">
 %s
 </ul>
 </body>
-</html>'''
+</html>
+"""
+date=""
+lis = ""
+img=""
 
-line1=a.split("\n")[0]
 lines=a.split("\n")
+line1=lines[0]
 
 #extract name of first person
 name1=line1.split("-")[1:]
 name1="-".join(name1)
 name1=name1.split(":")[0].strip()
 
-date1=""
 msg=""
-lis=""
+time1=""
+word=""
+img=""
 
 #traversing chat line-wise
 for line in lines:
+	
+	#checking if continued message in new line
+	x=line.split("-")[0]
+	try:
+		x=x.split(",")[1].strip()
+		if x[-2:]!="AM" and x[-2:]!="PM":
+			msg=msg+ "<br/>" + line
+			continue
+	except IndexError:
+		msg=msg+ "<br/>" + line
+		continue
+		
+	imgtag=""
+	name=line.split("-")[1:]
+	name="-".join(name)
+	name=name.split(":")[0].strip()
+
+	#extracting date of message and checking if date is new
+	date1=line.split(",")[0]
+	if (date1!=date):
+		date=date1	
+		word=line.split(":")[2:]
+		lis += '<li class="date"><span>%s</span></li>' % (date)
+
+	#extracting time
+	time1=line.split(",")[1:]
+	time1=",".join(time1)
+	time1=time1.split("-")[0].strip()
 
 	#extracting message
 	word=line.split(":")[2:]
 	word=":".join(word).strip()
 
-	#extracting name of person
-	name=line.split("-")[1:]
-	name="-".join(name)
-	name=name.split(":")[0].strip()
-	
-	#class allotment for css
-	if (name==name1):
-		clas="a"
-	else:
-		clas="b"
-
-	#extracting time
-	time=line.split(",")[1:]
-	time=",".join(time)
-	time=time.split("-")[0].strip()
-
-	#checking if continued message in new line
-	try:
-		if(time[-2:]!="AM" and time[-2:]!="PM"):
-			msg=msg+"<br/>"+line
-			continue
-	except:
-		msg=msg+"<br/>"+line
-		continue
-	
-	#extracting date of message and checking if date is new
-	date=line.split(",")[0]
-	if (date!=date1):
- 		date1=date
- 		lis+='<li class=%s>%s</li>'%("date",date)
-	
 	#checking for image or video as message
-
-	img=line.split(":")[2:]
-	img=":".join(img).strip()
-	img=img.split(" ")[0].strip()
-	#print img
-	imgtag=""
+	img=word.split(" ")[0]
 	
-	if (img[:3]=="IMG" and img [-4:]==".jpg"): #terminal numbers not included
-		imgtag='<img src='+os.path.join(sys.argv[1],img)+' width="250" border="1">'
-		imgtag='<a href='+os.path.join(sys.argv[1],img)+'>'+imgtag+'</a>'
-		#print word
-
-	if (img[:3]=="VID" and img [-4:]==".mp4"):
+	if img[0:3]=='IMG' and img[-4:]=='.jpg':
+		imgtag='<img src='+os.path.join(sys.argv[1],img)+' width="200px" border="1">'
+		imgtag ="<a target='_blank' href=" + os.path.join(sys.argv[1],img) + ">%s</a>" % imgtag
+	
+	if img[0:3]=='VID' and img[-4:]=='.mp4':
 		imgtag='<video width="320" height="240" controls> \
-  				<source src='+os.path.join(sys.argv[1],img)+' type="video/mp4">\
-  				</video>'
+  						<source src='+os.path.join(sys.argv[1],img)+' type="video/mp4"> \
+ 				</video>'
+
+ 	#class allotment for css	
+	clas=""
+	if (name==name1):
+		clas="b"
+	else:
+		clas="c"
 
 	#adding message to list depending on what type of message is it
 
+	t="time"
+
 	#for multiline message
-	if (msg!=""):
-		lis+='<li class=%s><span class=%s>%s<span class="time">%s</span></span></li>'%(clas,clas,msg,time)
-		
+	if msg:
+		lis += '<li class=%s><span class=%s>%s<span class=%s>%s</span></span></li>' %(clas,clas,msg,t,time1)
 	msg=""
 
-	#for image or video message
-	if (imgtag!=""):
-		lis+='<li class=%s>%s<span class="time">%s</span></li>'%(clas,imgtag,time)
-		
 	#for one-line message
-	if (msg=="" and imgtag==""):
-		lis+='<li class=%s><span class=%s>%s<span class="time">%s</span></span></li>'%(clas,clas,word,time)
+	if (imgtag==""):
+		lis += '<li class=%s><span class=%s>%s<span class=%s>%s</span></span></li>' %(clas,clas,word,t,time1)
+	
+	#for image or video message
+	else:
+		lis += '<li class=%s>%s<span class=%s>%s</span></li>' %(clas,imgtag,t,time1)
 
 #creating html file with require display properties of message
-output=template % lis
-fp=open("output.html","w")
+output = template % lis
+fp = open("output.html", "w")
 fp.write(output)
-webbrowser.open_new_tab("file://"+os.getcwd()+"/output.html")
+webbrowser.open_new("file://" + os.getcwd() + "/output.html")
 fp.close()
